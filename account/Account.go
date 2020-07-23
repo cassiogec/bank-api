b077d02cb6b995f916a0cc2f418cb82649788402
Cássio Eckstein <cassiogec@gmail.com>
2020-07-23 18:07:08 -0300

    renaming accounts folder

---
+++ account/Account.go
@@ -0,0 +1,14 @@
+package account
+
+import (
+	"net/http"
+)
+
+func AllAccounts(w http.ResponseWriter, r *http.Request) {
+}
+
+func AccountBalance(w http.ResponseWriter, r *http.Request) {
+}
+
+func NewAccount(w http.ResponseWriter, r *http.Request) {
+}
