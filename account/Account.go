bcadc3c6734307a2d998e6c922c85298f918f7df
Cássio Eckstein <cassiogec@gmail.com>
2020-07-23 18:36:05 -0300

    setting account functions as not implemented

--- account/Account.go
+++ account/Account.go
@@ -5,10 +5,13 @@ import (
 )
 
 func AllAccounts(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func AccountBalance(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func NewAccount(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
 }
