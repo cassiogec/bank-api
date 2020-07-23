74e3727d156ca8d7673a1d3815d44d2fa33db1cc
Cássio Eckstein <cassiogec@gmail.com>
2020-07-23 18:42:57 -0300

    setting transfer handlres as not implemented

---
+++ transfer/Transfer.go
@@ -0,0 +1,13 @@
+package transfer
+
+import (
+	"net/http"
+)
+
+func AllTransfers(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func NewTransfer(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
