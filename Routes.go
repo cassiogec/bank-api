92c9e389f72c0b646c4b059e957773343b6c171c
Cássio Eckstein <cassiogec@gmail.com>
2020-07-23 18:20:49 -0300

    renaming mux to router

--- Routes.go
+++ Routes.go
@@ -9,25 +9,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func SetupRoutes(mux *mux.Router) {
-	setupGenericRoutes(mux)
-	setupAccountRoutes(mux)
-	setupTransferRoutes(mux)
-	setupLoginRoutes(mux)
+func SetupRoutes(router *mux.Router) {
+	setupGenericRoutes(router)
+	setupAccountRoutes(router)
+	setupTransferRoutes(router)
+	setupLoginRoutes(router)
 }
 
-func setupGenericRoutes(mux *mux.Router) {
-	mux.HandleFunc("/", homepage).Methods("GET")
+func setupGenericRoutes(router *mux.Router) {
+	router.HandleFunc("/", homepage).Methods("GET")
 }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bank-Api")
 }
 
-func setupAccountRoutes(mux *mux.Router) {
-	mux.HandleFunc("/accounts", account.AllAccounts).Methods("GET")
-	mux.HandleFunc("/user/{account_id}/balance", account.AccountBalance).Methods("GET")
-	mux.HandleFunc("/accounts", account.NewAccount).Methods("POST")
+func setupAccountRoutes(router *mux.Router) {
+	router.HandleFunc("/accounts", account.AllAccounts).Methods("GET")
+	router.HandleFunc("/user/{account_id}/balance", account.AccountBalance).Methods("GET")
+	router.HandleFunc("/accounts", account.NewAccount).Methods("POST")
 }
 
 func setupTransferRoutes(mux *mux.Router) {
