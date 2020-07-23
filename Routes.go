fd843e058ebf4477da50a644d7650a188397a06c
Cássio Eckstein <cassiogec@gmail.com>
2020-07-23 18:15:06 -0300

    setting up accouts routes

--- Routes.go
+++ Routes.go
@@ -1,15 +1,33 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"net/http"
+
+	"bank-api/account"
+
+	"github.com/gorilla/mux"
+)
 
 func SetupRoutes(mux *mux.Router) {
-
+	setupGenericRoutes(mux)
+	setupAccountRoutes(mux)
+	setupTransferRoutes(mux)
+	setupLoginRoutes(mux)
 }
 
 func setupGenericRoutes(mux *mux.Router) {
+	mux.HandleFunc("/", homepage).Methods("GET")
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bank-Api")
 }
 
 func setupAccountRoutes(mux *mux.Router) {
+	mux.HandleFunc("/accounts", account.AllAccounts).Methods("GET")
+	mux.HandleFunc("/user/{account_id}/balance", account.AccountBalance).Methods("GET")
+	mux.HandleFunc("/accounts", account.NewAccount).Methods("POST")
 }
 
 func setupTransferRoutes(mux *mux.Router) {
