6dbd48c68764e377a6a51e2fd682c347631277a0
Cássio Eckstein <cassiogec@gmail.com>
2020-07-26 17:51:09 -0300

    adding listener for the server on base file and calling it from main

--- controllers/base.go
+++ controllers/base.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/cassiogec/bank-api/models"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 
@@ -26,5 +28,14 @@ func (server *Server) Initialize(DbUser, DbPassword, DbPort, DbHost, DbName stri
 		log.Fatal("This is the error:", err)
 	}
 
+	server.DB.Debug().AutoMigrate(&models.Account{})
+
 	server.Router = mux.NewRouter()
+
+	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("Listening to port 8080")
+	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/cassiogec/bank-api/controllers"
 	"github.com/joho/godotenv"
 )
+
+var server = controllers.Server{}
 
 func main() {
 	var err error
@@ -12,4 +16,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error getting env, %v", err)
 	}
+
+	server.Initialize(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	server.Run(":8080")
 }
