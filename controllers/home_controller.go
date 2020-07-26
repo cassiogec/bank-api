50c89cd883b8ff2ad5fd15faa922003bc5be61a2
Cássio Eckstein <cassiogec@gmail.com>
2020-07-26 17:54:07 -0300

    creating home_controller handler

---
+++ controllers/home_controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/cassiogec/bank-api/responses"
+)
+
+type Message struct {
+	message string
+}
+
+func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, Message{message: "Welcome to the Bank API"})
+}
