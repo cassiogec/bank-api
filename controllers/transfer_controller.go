a90738aa389f9b396f1fcc482400674f24f44aeb
Cássio Eckstein <cassiogec@gmail.com>
2020-07-26 18:22:38 -0300

    implementing all transfer handler

--- controllers/transfer_controller.go
+++ controllers/transfer_controller.go
@@ -2,10 +2,21 @@ package controllers
 
 import (
 	"net/http"
+
+	"github.com/cassiogec/bank-api/models"
+	"github.com/cassiogec/bank-api/responses"
 )
 
-func AllTransfers(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+func (server *Server) AllTransfers(w http.ResponseWriter, r *http.Request) {
+
+	transfer := models.Transfer{}
+
+	transfers, err := transfer.FindAllTransfers(server.DB)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	responses.JSON(w, http.StatusOK, transfers)
 }
 
 func NewTransfer(w http.ResponseWriter, r *http.Request) {
