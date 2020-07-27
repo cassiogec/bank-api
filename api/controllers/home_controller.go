7163d5caa41ab2cac9c909b80f5680f908fb4443
Cássio Eckstein <cassiogec@gmail.com>
2020-07-27 20:43:03 -0300

    fixing home mandler

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -11,5 +11,5 @@ type Message struct {
 }
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, http.StatusOK, Message{message: "Welcome to the Bank API"})
+	responses.JSON(w, http.StatusOK, "Welcome to the Bank API")
 }
